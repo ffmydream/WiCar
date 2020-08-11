2a34a001656394e68d2f1326b7ca7084f3264548
ffmydream <232380142@qq.com>
2020-08-11 12:16:20 +0800

    OK

--- app/src/main/java/com/example/wicar/MainActivity.kt
+++ app/src/main/java/com/example/wicar/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.wicar
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
@@ -10,14 +9,17 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import java.io.PrintWriter
 import java.net.Socket
-import kotlin.coroutines.CoroutineContext
 
+object SrvAngle {
+    var hSrvAngle = 90
+    var vSrvAngle = 90
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val ch= Channel<String>()
+        val ch = Channel<String>(0)
         val url = "http://v.163.com/paike/V8H1BIE6U/VAG52A1KT.html"
 
         webView.settings.apply {
@@ -29,28 +31,94 @@ class MainActivity : AppCompatActivity() {
             setSupportZoom(true)
             javaScriptCanOpenWindowsAutomatically = false
         }
-        webView.webViewClient=                WebViewClient()
+        webView.webViewClient = WebViewClient()
         webView.loadUrl(url)
         CoroutineScope(Dispatchers.IO).launch {
 
             val os = Socket("192.168.1.2", 50000).getOutputStream()
             val pw = PrintWriter(os)
-            while (true){
+            while (true) {
                 pw.write(ch.receive())
                 pw.flush()
             }
         }
 
-          leftButton.setOnTouchListener(RepeatListener(300, 300,
-              View.OnClickListener {
-                CoroutineScope(Dispatchers.IO).launch{
-                    ch.send("car:leftMotor:100")
-                }
-              }))
+        leftButton.setOnTouchListener(
+            RepeatListener(300, 300,
+                View.OnClickListener {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        ch.send("car:leftMotor:100")
+                    }
+                })
+        )
 
-}
+        rightButton.setOnTouchListener(
+            RepeatListener(300, 300,
+                View.OnClickListener {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        ch.send("car:rightMotor:100")
+                    }
+                })
+        )
+        forButton.setOnTouchListener(
+            RepeatListener(300, 300,
+                View.OnClickListener {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        ch.send("car:forMotor:100")
+                    }
+                })
+        )
+        backButton.setOnTouchListener(
+            RepeatListener(300, 300,
+                View.OnClickListener {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        ch.send("car:backMotor:100")
+                    }
+                })
+        )
+        srvLeftButton.setOnTouchListener(
+            RepeatListener(300, 300,
+                View.OnClickListener {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        if (SrvAngle.hSrvAngle > 3)
+                            SrvAngle.hSrvAngle = SrvAngle.hSrvAngle - 3
+                        ch.send("srv:left:${SrvAngle.hSrvAngle}")
+                    }
+                })
+        )
+        srvRightButton.setOnTouchListener(
+            RepeatListener(300, 300,
+                View.OnClickListener {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        if (SrvAngle.hSrvAngle < 177)
+                            SrvAngle.hSrvAngle = SrvAngle.hSrvAngle + 3
+                        ch.send("srv:right:${SrvAngle.hSrvAngle}")
+                    }
+                })
+        )
+        srvUpButton.setOnTouchListener(
+            RepeatListener(300, 300,
+                View.OnClickListener {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        if (SrvAngle.vSrvAngle < 170)
+                            SrvAngle.vSrvAngle = SrvAngle.vSrvAngle + 3
+                        ch.send("srv:up:${SrvAngle.vSrvAngle}")
+                    }
+                })
+        )
+        srvDownButton.setOnTouchListener(
+            RepeatListener(300, 300,
+                View.OnClickListener {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        if (SrvAngle.vSrvAngle > 60)
+                            SrvAngle.vSrvAngle = SrvAngle.vSrvAngle - 3
+                        ch.send("srv:down:${SrvAngle.vSrvAngle}")
+                    }
+                })
+        )
 
 
+    }
 
 
 }
